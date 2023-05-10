6c7450482cf1fd1814f6d08d377ebaf667d178bc
Kushpriya <priyakushawaha73@gmail.com>
2023-05-10 20:18:33 +0545

    final

---
+++ src/Pacman/Pacman.java
@@ -0,0 +1,17 @@
+package Pacman;
+import javax.swing.*;
+
+public class Pacman extends JFrame {
+    public Pacman() {
+        add(new Model());
+    }
+    public static void main(String[] args) {
+        Pacman pacman = new Pacman();
+        pacman.setVisible(true);
+        pacman.setTitle("Pacman");
+        pacman.setSize(380, 420);
+        pacman.setResizable(false);
+        pacman.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pacman.setLocationRelativeTo(null);
+    }
+}
